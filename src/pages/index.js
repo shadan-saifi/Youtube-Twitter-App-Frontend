8ea38b8f2667bfc097c259f6131a70e9763d73f5
Shadan Saifi <shadansaif99@gmail.com>
2024-03-10 12:50:13 +0530

    add

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,5 @@ import HomePage from "./HomePage";
 import CreateAccountPage from "./CreateAccountPage";
 import UserVideosPage from "./userPages/UserVideosPage"
 import UserPage from "./userPages/UserPage";
-import VideoByIdPage from "./videoPages/videoByIdPage";
+import VideoByIdPage from "./videoPages/VideoByIdPage";
 export {LoginPage,HomePage,CreateAccountPage, UserVideosPage, UserPage, VideoByIdPage}
