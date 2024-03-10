19ecd510463e5027d3b95512a78941a41caa1e47
Shadan Saifi <shadansaif99@gmail.com>
2024-03-10 13:28:57 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
    proxy:{
-    '/api': 'http://localhost:8000'
+    // '/api': 'http://localhost:8000'
+    "/api": "https://you-tube-twitter-app.vercel.app"
    },
   //  port: 8000,
   },
