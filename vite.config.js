c988dad513a9410bead176c1490de9e41d23ba5d
Shadan Saifi <shadansaif99@gmail.com>
2024-03-10 14:08:55 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server:{
-  //  proxy:{
-  //   // '/api': 'http://localhost:8000'
-  //   "/api": 'https://you-tube-twitter-app.vercel.app'
-  //  },
-  // //  port: 8000,
-  // },
+  server:{
+   proxy:{
+    '/api': 'http://localhost:8000'
+    // "/api": 'https://you-tube-twitter-app.vercel.app'
+   },
+  //  port: 8000,
+  },
   plugins: [react()],
 })
