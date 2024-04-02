6caf1afe1bff1df9e2ee5820e6e4b415fb8018d5
Shadan Saifi <shadansaif99@gmail.com>
2024-04-02 17:41:25 +0530

    apiroutechanged

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: `${process.env.DOMAIN}/api/v1/users`,
+    baseURL: `https://you-tube-twitter-app.vercel.app/api/v1/users`,
     headers: {
       "Content-Type": "application/json",
     },
