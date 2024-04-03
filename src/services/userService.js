a3fb486894eedb23807ad635dfcf6624a2cd9516
Shadan Saifi <shadansaif99@gmail.com>
2024-04-03 14:31:08 +0530

    add

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-axios.defaults.baseURL =  `${import.meta.env.DOMAIN}/api/v1/users`;
+axios.defaults.baseURL =  `${import.meta.env.VITE_API_URL}/api/v1/users`;
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 
